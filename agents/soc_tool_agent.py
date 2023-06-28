46875f42862fc44ec325ea4d68483bcaff860400
zhouxy <150273112@qq.com>
2023-06-28 20:10:22 +0800

    提交加入2个chain

---
+++ agents/soc_tool_agent.py
@@ -0,0 +1,38 @@
+from langchain.agents import (
+    AgentExecutor,
+    LLMSingleActionAgent,
+)
+from langchain import OpenAI, LLMChain
+from typing import List, Union
+from chains.tooslsOutputParser import ToolsOutputParser
+from prompts.prompt import (CustomPromptTemplate, template)
+from tools.toolRetriever import get_tools
+
+
+llm = OpenAI(temperature=0)
+
+output_parser = ToolsOutputParser()
+
+prompt = CustomPromptTemplate(
+    template=template,
+    tools_getter=get_tools,
+    # This omits the `agent_scratchpad`, `tools`, and `tool_names` variables because those are generated dynamically
+    # This includes the `intermediate_steps` variable because that is needed
+    input_variables=["input", "intermediate_steps"],
+)
+
+# LLM chain consisting of the LLM and a prompt
+llm_chain = LLMChain(llm=llm, prompt=prompt)
+
+tools = get_tools("whats the weather?")
+tool_names = [tool.name for tool in tools]
+agent = LLMSingleActionAgent(
+    llm_chain=llm_chain,
+    output_parser=output_parser,
+    stop=["\nObservation:"],
+    allowed_tools=tool_names,
+)
+
+agent_executor = AgentExecutor.from_agent_and_tools(
+    agent=agent, tools=tools, verbose=True
+)
