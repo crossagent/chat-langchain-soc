1c8690afaca73f2d0c790958f25a494210e1f12c
crossagent <150273112@qq.com>
2023-07-03 14:11:57 +0800

    新增专家寻找模块

--- chat.py
+++ chat.py
@@ -1,7 +1,10 @@
 from langchain.chat_models import ChatOpenAI
-from agents.soc_gpt_agent import SocGPT
-from langchain.callbacks.base import  AsyncCallbackHandler
-from langchain.callbacks.manager import  AsyncCallbackManager
+from agents.soc_gpt_agent import get_agent
+from langchain.callbacks.base import AsyncCallbackHandler
+from langchain.callbacks.manager import AsyncCallbackManager
+from langchain.agents import AgentExecutor
+from typing import Any, Dict, List
+from agents.soc_module_answer_agent import get_expert_answer
 
 # Set up of your agent
 
@@ -37,15 +40,43 @@ config = dict(
 
 def get_soc_chain(
     question_handler:AsyncCallbackHandler = None, stream_handler:AsyncCallbackHandler = None, tracing: bool = False
-) -> SocGPT:
+) -> AgentExecutor:
     """Get the chain."""
     # init llm
     steam_manager = AsyncCallbackManager([stream_handler])
     llm = ChatOpenAI(temperature=0, streaming=True, callback_manager=steam_manager)
 
-    soc_agent = SocGPT.from_llm(llm, verbose=False, **config)
-
-    # init sales agent
-    soc_agent.seed_agent()
+    soc_agent = get_agent(llm, verbose=False, callback_manager=[question_handler])
 
     return soc_agent
+
+def get_soc_gpt(
+    tracing: bool = False, 
+    verbose: bool = False,
+    **kwargs: Any
+) -> AgentExecutor:
+    """Get the chain."""
+    # init llm
+    steam_manager = AsyncCallbackManager([kwargs['stream_handler']])
+    llm = ChatOpenAI(temperature=0, streaming=True, callback_manager=steam_manager)
+
+    question_manager = AsyncCallbackManager(kwargs['question_handler'])
+    soc_agent = get_agent(llm, verbose=verbose, callback_manager=[question_manager])
+
+    return soc_agent
+
+
+
+async def runTest():
+    from callbacks.socConsoleCallBacks import QuestionGenCallbackHandler,StreamingLLMCallbackHandler
+
+    question_handler = QuestionGenCallbackHandler()
+    stream_handler = StreamingLLMCallbackHandler()
+
+    config = {'stream_handler' : stream_handler, 'question_handler' : question_handler}
+    agent_executor = get_soc_gpt(tracing=False, verbose=True, **config)
+    
+    result = await agent_executor.arun("狙击枪无法使用了")
+
+import asyncio
+result = asyncio.run(runTest())
